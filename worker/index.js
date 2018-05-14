17f8defd2b4a2e2b2507e4f12d55d9572a492d84
xmaga <xmaga@berlingskemedia.dk>
2018-05-14 12:44:48 +0200

    SQL query to MDB - work in progress.

--- worker/index.js
+++ worker/index.js
@@ -6,16 +6,16 @@ function run() {
     .then(surveys => {
       const mdbRequests = surveys.map(({survey, responses}) => {
         const payload = getMdbPayload(survey, responses);
-        // TODO: build sql from payload
-        console.log(payload.contactEmail);
-        return Promise.resolve();
         // TODO: perform mdb request for this payload
-        return mdb.query('INSERT...');
+        return sendPayloadToMdb(payload);
       });
 
       Promise.all(mdbRequests).then(() => {
         // TODO: clean up mdb after all requests
+
+
         // TODO: schedule next walk
+        setTimeout(() => run(), 12 * 60 * 60 * 1000);
       });
     })
   .catch(err => {
@@ -39,9 +39,7 @@ function findContactEmail(response) {
           return subAnswer.answer;
         }
       }
-    }
-
-    if (isValidEmail(answer.answer)) {
+    } else if (isValidEmail(answer.answer)) {
       return answer.answer;
     }
   }
@@ -71,6 +69,7 @@ function getPagedResults(getter, pageSize) {
     getter(pageSize, 1).then(firstResponse => {
       const requests = [Promise.resolve(firstResponse)];
 
+      // TODO: count to firstResponse.total_pages
       // for (let i = 2, n = firstResponse.total_pages; i < n; i++) {
       for (let i = 2, n = 1; i < n; i++) {
         requests.push(getter(pageSize, i));
@@ -102,93 +101,11 @@ function getSurveysFromGizmo() {
   });
 }
 
+function sendPayloadToMdb(payload) {
+  const tableName = 'tbl_surveygizmo';
+  const query = `DELETE from ${tableName} WHERE survey_id=${payload.surveyId};`;
 
-function itereateSurveryLists({lists, index = 0}){
-  // console.log(lists);
-  return 1;
-  const list = lists[index];
-
-  function listComplete() {
-    if (++index === lists.length) {
-      console.log('All lists have been cycled through');
-      return Promise.resolve()
-    } else {
-      return itereateSurveryLists({lists: lists, index: index});
-    }
-  }
-
-  return getSurveyContacts({surveyId: list.id})
-  .then(contacts => doSomeStuff({contacts: contacts}))
-  .then(() => listComplete());
-
-}
-
-
-function getSurveyContacts({surveyId}){
-  const fakeData = [
-      {
-      surveyId: 1212,
-      surveyName: 'DSADA',
-      contactId: 112123,
-      contactEmail: 'ddas@fdsfsd.dk',
-      date: new Date()
-    }
-  ];
-
-  return Promise.resolve(fakeData);
-}
-
-
-function doSomeStuff({contacts, index = 0}){
-
-  const contact = contacts[index];
-
-  function contactsComplete() {
-    if (++index === contacts.length) {
-      console.log('All contacts have been cycled through');
-      return Promise.resolve()
-    } else {
-      return doSomeStuff({contacts: contacts, index: index});
-    }
-  }
-
-  // This will me an UPSERT where we also set our own date for the last insert/update
-
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      // Here we update the MDB database
-      console.log(`This user ${contact.contactId} has been updated`);
-      return resolve();
-    }, 1000);
-  });
-}
-
-
-function cleanUpOldRows(){
-
-  // Here we will delete all rows that have not been UPSERT in the last 3 months
-
-  const some_shitty_data = 'sds';
-  const sql = `DELETE FROM tbl_survers WHERE dsdsdsds = ${some_shitty_data}`;
-  // return mdb.query(sql);
-  console.log('Deleting');
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      // Here we update the MDB database
-      console.log(`All old rows have been deleted`);
-      return resolve();
-    }, 2000);
-  });
-}
-
-
-function waitUntil({timeout}){
-  console.log(`Waiting ${timeout/1000} seconds`);
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      return resolve();
-    }, timeout);
-  });
+  return Promise.resolve();
 }
 
 run();
